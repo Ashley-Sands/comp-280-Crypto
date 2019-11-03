2938d0f5eda1dc2c86371b85fb809fbae589b097
Ashley Sands <ashsands1@amstrike.com>
2019-11-03 19:43:54 +0000

    added encryption test, using unittest
    its only the basic class setup

---
+++ encryption_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+class CeasarTest( unittest.TestCase ):
+    pass
+
+
+class XorTest( unittest.TestCase ):
+    pass
+
+
+class AesCryptographyTest( unittest.TestCase ):
+    pass
+
+
+class Aes__Test( unittest.TestCase ):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main();
