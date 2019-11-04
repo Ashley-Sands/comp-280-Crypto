0905c8e6e451a81ecd61e270c8698bb984ba2390
Ashley Sands <ashsands1@amstrike.com>
2019-11-04 00:29:15 +0000

    Added AES test

--- encryption_test.py
+++ encryption_test.py
@@ -3,7 +3,6 @@ import bruteForceMethods
 import time
 
 
-
 class BaseTestClass( unittest.TestCase ):
 
     def setUp(self):
@@ -169,13 +168,66 @@ class XorTest( BaseTestClass ):
         possible_strings = brute_force.xor( encrypted_string, brute_force_max_key )
         self.assertTrue( (cipher_key, string_to_encrypt) in possible_strings )
 
-class AesCryptographyTest( unittest.TestCase ):
+class AesCryptographyTest( BaseTestClass ):
 
     import aes
 
-    def test_string_is_not_equal_to_aes_encrypted_string(self):
-        pass
+    def test_string_is_not_equal_to_encrypted_string(self):
 
+        # setup cipher
+        cipher = self.aes.AES(16)
+
+        # encrypt string
+        string_to_encrypt = "Helloo World"
+
+        encrypted_string = cipher.encrypt( string_to_encrypt )
+
+        self.assertNotEqual( string_to_encrypt, encrypted_string )
+
+    def test_decrypted_string_is_equals_to_string_to_encrypt(self):
+
+        # setup cipher
+        cipher = self.aes.AES(16)
+
+        # encrypt string
+        string_to_encrypt = "Helloo World"
+
+        encrypted_string = cipher.encrypt( string_to_encrypt )
+        decrypted_string = cipher.decrypt( encrypted_string )
+
+        self.assertEqual( decrypted_string.decode('utf-8'), string_to_encrypt )
+
+    def test_decrypted_with_different_key_does_not_equal_string_to_encrypt(self):
+
+        # set up encrypt cipher
+        encrypt_cipher = self.aes.AES(16)
+
+        # setup decrypt cipher
+        decrypt_cipher = self.aes.AES(16)
+
+        # encrypt string
+        string_to_encrypt = "Helloo World"
+
+        encrypted_string = encrypt_cipher.encrypt(string_to_encrypt)
+        decrypted_string = decrypt_cipher.decrypt(encrypted_string)
+
+        self.assertNotEqual( decrypted_string, string_to_encrypt )
+
+    def test_brute_force_decryption_method_finds_string_to_encryption(self):
+        # I am not going to implement this here.
+        # if there are 255 different bytes (python bytes function)
+        # and we use a 128bit key (or 16 bytes) that would mean
+        # that there are 255 to the power of 16 different keys
+        # ‭or 319,626,579,315,078,487,616,775,634,918,21‬ possibilities
+        # (AKA a very, very, very, very big number)
+        # ...
+        # If i done my math correctly, on my PC (using single thread)
+        # it would take ~9.628519811583301e+21 Millenniums (+/- 10%)
+        # see implementation in main.py for me info on the math :)
+        # ...
+        # So if i implemented it here we would all be long gone before
+        # the test finished.
+        pass
 
 class Aes__Test( unittest.TestCase ):
     pass
