012ad4e94febfb10809d308cd18aae256250a868
Ashley Sands <ashsands1@amstrike.com>
2019-11-04 19:18:47 +0000

    Prevent all timings being printed
    theres now a timing threshold

--- encryption_test.py
+++ encryption_test.py
@@ -5,12 +5,15 @@ import time
 
 class BaseTestClass( unittest.TestCase ):
 
+    print_elapsed_time_threshold = -1   # < 0 always print else print if > value (sec)
+
     def setUp(self):
         self.started_time = time.time()
 
     def tearDown(self):
         self.elapsed_time = time.time() - self.started_time
-        print( '.'.join('{} ( {}s )'.format( self.id(), round( self.elapsed_time, 3 ) ).split('.')[1:]) )
+        if self.elapsed_time > self.print_elapsed_time_threshold:
+            print( '.'.join('{} ( {}s )'.format( self.id(), round( self.elapsed_time, 3 ) ).split('.')[1:]) )
 
 
 class CaesarTest( BaseTestClass ):
