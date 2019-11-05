b600eddfd98589990addac17b085ff2c5965f7e8
Ashley Sands <ashsands1@amstrike.com>
2019-11-05 23:21:44 +0000

    added set key len and chr range

--- flipFlop.py
+++ flipFlop.py
@@ -1,12 +1,13 @@
 
 class FlipFlop:
 
-    def __init__(self, min_chr, max_chr, str_len):
+    def __init__(self, min_chr, max_chr, str_len, bytes_=False):
 
         self.min_chr = min_chr
         self.max_chr = max_chr
         self.str_length = str_len
         self.current_col = str_len - 1
+        self.bytes = bytes_
 
         self.out = [chr(self.min_chr)] * self.str_length
         self.skip = True
@@ -35,4 +36,14 @@ class FlipFlop:
 
         print(self.out)
 
-        return ''.join(self.out).encode("utf-8")
+        if self.bytes:
+            return ''.join(self.out).encode("utf-8")
+        else:
+            return ''.join(self.out)
+
+    def set_key_len(self, len):
+        self.str_length = len
+
+    def set_chr_range(self, min_c, max_c):
+        self.min_chr = min_c
+        self.max_chr = max_c
